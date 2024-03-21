dfce8f1d2c35827102ec4d6fed6e22e3c3b1f3ef
CesarAlva <66078322+cesaralva@users.noreply.github.com>
2024-03-21 17:49:43 -0500

    first Commit microservice

--- microservice.course/src/main/java/com/microservice/course/controllers/CourseController.java
+++ microservice.course/src/main/java/com/microservice/course/controllers/CourseController.java
@@ -1,4 +1,37 @@
 package com.microservice.course.controllers;
 
+import com.microservice.course.entities.Course;
+import com.microservice.course.service.ICourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/course")
 public class CourseController {
+
+    @Autowired
+    private final ICourseService iCourseService;
+
+    public CourseController(ICourseService iCourseService) {
+        this.iCourseService = iCourseService;
+    }
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveCourse(@RequestBody Course course){
+        iCourseService.save(course);
+
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> findAllCourse(){
+        return ResponseEntity.ok(iCourseService.findAll());
+    }
+
+    @GetMapping("/searche/{id}")
+    public ResponseEntity<?>finById(@PathVariable Long id){
+        return ResponseEntity.ok(iCourseService.findById(id));
+    }
 }
