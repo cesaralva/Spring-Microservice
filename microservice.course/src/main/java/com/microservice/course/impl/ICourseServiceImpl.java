375c9acaf39f082fdfd2b4090e3b91380d918129
CesarAlva <66078322+cesaralva@users.noreply.github.com>
2024-03-21 17:40:11 -0500

    first Commit microservice

--- microservice.course/src/main/java/com/microservice/course/impl/ICourseServiceImpl.java
+++ microservice.course/src/main/java/com/microservice/course/impl/ICourseServiceImpl.java
@@ -24,7 +24,7 @@ public class ICourseServiceImpl implements ICourseService {
 
     @Override
     public Course findById(Long id) {
-        return (Course) iCourseRepository.findAll(id);
+        return iCourseRepository.findById(id).orElseThrow();
     }
 
     @Override
